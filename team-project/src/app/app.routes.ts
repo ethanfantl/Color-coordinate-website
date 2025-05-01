2147bc8a6748e77d375adc7bd45c5adf35e84f22
Troy-L <troylee@colostate.edu>
2025-05-01 13:50:20 -0600

    Added basic for colors page and add color input framework

--- team-project/src/app/app.routes.ts
+++ team-project/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ColorsComponent } from './pages/colors/colors.component';
 
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-    
+    { path: 'colors', component: ColorsComponent },
 ];
