dc65aadd82690dac34b808b2179507d5abe88099
Troy-L <troylee@colostate.edu>
2025-05-01 15:08:26 -0600

    Added framework for color operations

--- team-project/src/app/app.routes.ts
+++ team-project/src/app/app.routes.ts
@@ -2,12 +2,10 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ColorDatabaseComponent } from './pages/color-database/color-database.component';
-import { ColorsComponent } from './pages/colors/colors.component';
 
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'color-database', component: ColorDatabaseComponent },
-    { path: 'colors', component: ColorsComponent },
+    { path: 'color-database', component: ColorDatabaseComponent }
 ];
--- team-project/src/app/pages/color-database/color-database.component.ts
+++ team-project/src/app/pages/color-database/color-database.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Import CommonModule for NgIf/NgFor
 import { FormsModule } from '@angular/forms';   // Import FormsModule for ngModel
 import { colorStorage } from '../../misc/colorStorage.model'; 
+import { IndentStyle } from 'typescript';
 
 @Component({
   selector: 'app-color-database',
@@ -30,6 +31,45 @@ export class ColorDatabaseComponent implements OnInit{
   private saveColorsToSessionStorage(colorsToSave: colorStorage[]): void {
     sessionStorage.setItem('password', JSON.stringify(colorsToSave));
     this.loadColorsFromSessionStorage();
+  }
+
+  createColorStorage(name: string, hex: string, id: number): colorStorage {
+    return {
+      id,
+      name,
+      hex,
+    };
+  }
+
+  addColor(name: string, hex: string, id: string): void {
+    let idUsed = false;
+    for(let color in this.colorsDatabase){
+      if(color[0] == id){
+        idUsed = true;
+      }
+    }
+    if(idUsed){
+
+    }
+    else{
+      this.colorsDatabase.push(this.createColorStorage(name, hex.toUpperCase(), Number(id)));
+  
+    }
+  }
+
+  removeColor(color: colorStorage): void {
+    let removeIndex = this.colorsDatabase.indexOf(color);
+    this.colorsDatabase.splice(removeIndex, 1);
+  }
+
+  changeName(color: colorStorage, newName: string): void {
+    let changeIndex = this.colorsDatabase.indexOf(color);
+    this.colorsDatabase[changeIndex].name = newName;
+  }
+
+  changeHex(color: colorStorage, newHex: string): void {
+    let changeIndex = this.colorsDatabase.indexOf(color);
+    this.colorsDatabase[changeIndex].hex = newHex.toUpperCase();
   }
 
 }
